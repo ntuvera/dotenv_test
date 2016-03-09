06cc4f772c45951378e6ab68984c45d2d90ee90c
ntuvera <ntuvera@gmail.com>
2016-03-09 13:14:39 -0500

    switch back to previous syntax

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var app = express();
 
 // app.use(bodyParser.urlencoded({ extended: false }))
  
-// var connection = new Sequelize(process.env.JAWSDB_URL || 'dotenv_db', 'root');
-var connection = new Sequelize(process.env.JAWSDB_URL);
+var connection = new Sequelize(process.env.JAWSDB_URL || 'dotenv_db', 'root');
+// var connection = new Sequelize(process.env.JAWSDB_URL);
 // var Character = require('./models/character.js');
 
 var Character = connection.define('Character', {
