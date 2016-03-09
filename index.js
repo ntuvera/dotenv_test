2f6faf4a4ad1c65b81cc9b3b1e2e1fe18c2de53d
ntuvera <ntuvera@gmail.com>
2016-03-09 12:37:54 -0500

    create connection and some console.logs

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 var express = require('express');
 var Sequelize = require('sequelize');
+var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-var connection = new Sequelize('dotenv_db', 'root');
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json 
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+});
+
+var connection = new Sequelize(process.env.JAWSDB_URL || 'dotenv_db', 'root');
 
 if (connection) { 
 	console.log('connection made')
 } else {
 	console.log("we've got nothin'")
 }
+
+// console.log("here's what's in process.env: " + JSON.stringify(process.env));
+console.log("here's what JAWSDBURL is: " + process.env.JAWSDB_URL)
+
+
 
 app.listen(PORT, function() {
 	console.log("Listening on: " + PORT);
